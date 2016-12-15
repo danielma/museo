3dae0a3cd2f5359808e8865f961d55dca940be28
Daniel Ma <drailskid@yahoo.com>
2016-12-15 11:41:51 -0800

    Share code

--- lib/museo/minitest_integration.rb
+++ lib/museo/minitest_integration.rb
@@ -1,5 +1,7 @@
 module Museo
   module MinitestIntegration
+    include TestIntegration
+
     def self.included(base)
       base.extend(ClassMethods)
     end
@@ -8,14 +10,13 @@ module Museo
       def snapshot(description)
         test "snapshot: #{description}" do
           begin
-            _add_snapshot_helper_methods
-            _redefine_render_with_snapshot_layout
+            _museo_setup
 
             instance_eval(&Proc.new)
 
             assert_snapshot
           ensure
-            _remove_snapshot_helper_methods
+            _museo_teardown
           end
         end
       end
@@ -27,45 +28,6 @@ module Museo
         Snapshot.sanitize_response(response.body),
         "Snapshot did not match",
       )
-    end
-
-    def _add_snapshot_helper_methods
-      @controller.class.helper do
-        Snapshot.stubbed_methods.each do |method_name, callable|
-          alias_method("_original_#{method_name}", method_name) if method_defined?(method_name)
-
-          define_method(method_name) do |*args, &block|
-            output = callable.respond_to?(:call) ? instance_exec(*args, block, &callable) : callable
-
-            [
-              "<!--",
-              "Stubbed method call: #{method_name}",
-              Snapshot.formatter.format(output),
-              "-->",
-              "",
-            ].join("\n").html_safe
-          end
-        end
-      end
-    end
-
-    def _remove_snapshot_helper_methods
-      @controller.class.helper do
-        Snapshot.stubbed_methods.each do |method_name, _callable|
-          remove_method method_name
-
-          if method_defined?("_original_#{method_name}")
-            alias_method method_name, "_original_#{method_name}"
-          end
-        end
-      end
-    end
-
-    def _redefine_render_with_snapshot_layout
-      @controller.define_singleton_method(:render) do |*args|
-        options = args.length > 1 ? args.last : {}
-        super(*args[1..-2], options.merge(layout: "museo/snapshot"))
-      end
     end
   end
 end
---
+++ lib/museo/test_integration.rb
@@ -0,0 +1,51 @@
+module Museo
+  module TestIntegration
+    def _museo_setup
+      _add_snapshot_helper_methods
+      _redefine_render_with_snapshot_layout
+    end
+
+    def _museo_teardown
+      _remove_snapshot_helper_methods
+    end
+
+    def _add_snapshot_helper_methods
+      @controller.class.helper do
+        Snapshot.stubbed_methods.each do |method_name, callable|
+          alias_method("_original_#{method_name}", method_name) if method_defined?(method_name)
+
+          define_method(method_name) do |*args, &block|
+            output = callable.respond_to?(:call) ? instance_exec(*args, block, &callable) : callable
+
+            [
+              "<!--",
+              "Stubbed method call: #{method_name}",
+              Snapshot.formatter.format(output),
+              "-->",
+              "",
+            ].join("\n").html_safe
+          end
+        end
+      end
+    end
+
+    def _remove_snapshot_helper_methods
+      @controller.class.helper do
+        Snapshot.stubbed_methods.each do |method_name, _callable|
+          remove_method method_name
+
+          if method_defined?("_original_#{method_name}")
+            alias_method method_name, "_original_#{method_name}"
+          end
+        end
+      end
+    end
+
+    def _redefine_render_with_snapshot_layout
+      @controller.define_singleton_method(:render) do |*args|
+        options = args.length > 1 ? args.last : {}
+        super(*args[1..-2], options.merge(layout: "museo/snapshot"))
+      end
+    end
+  end
+end
