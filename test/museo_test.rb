c62c04ae3f128f990799a500feb60a2101710014
Daniel Ma <drailskid@yahoo.com>
2016-12-14 16:36:22 -0800

    attempt to bring in rails 4 plugin structure

--- test/museo_test.rb
+++ test/museo_test.rb
@@ -1,11 +1,7 @@
-require 'test_helper'
+require "test_helper"
 
 class MuseoTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Museo::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
