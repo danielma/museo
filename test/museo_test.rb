6d2421dca0089cbc3542de0b857631fa3aec231c
Daniel Ma <drailskid@yahoo.com>
2016-12-16 09:22:06 -0800

    shared mutable state :upside_down_smile:

--- test/museo_test.rb
+++ test/museo_test.rb
@@ -2,34 +2,42 @@ require "test_helper"
 
 class MuseoTest < ActiveSupport::TestCase
   test ".configure add configuration" do
-    formatter = Object.new
+    with_modified_configuration do
+      formatter = Object.new
 
-    described_class.configure do |config|
-      config.stub(:render, nil)
-      config.formatter = formatter
+      described_class.configure do |config|
+        config.stub(:render, nil)
+        config.formatter = formatter
+      end
+
+      assert_equal 1, described_class.configuration.stubbed_methods.length
+      assert_equal formatter, described_class.configuration.formatter
     end
-
-    assert_equal 1, described_class.configuration.stubbed_methods.length
-    assert_equal formatter, described_class.configuration.formatter
   end
 
   test "clear_stubs!" do
-    described_class.configure do |config|
-      config.stub(:render, nil)
+    with_modified_configuration do
+      described_class.configure do |config|
+        config.stub(:render, nil)
+      end
+
+      assert_equal 1, described_class.configuration.stubbed_methods.length
+
+      described_class.configuration.clear_stubs!
+
+      assert_empty described_class.configuration.stubbed_methods
     end
-
-    assert_equal 1, described_class.configuration.stubbed_methods.length
-
-    described_class.configuration.clear_stubs!
-
-    assert_empty described_class.configuration.stubbed_methods
   end
 
   test "uses minitest test directory by default" do
     assert_includes described_class.pathname("hello").to_s, "test/snapshots/hello"
   end
 
-  teardown do
-    described_class.clear_configuration!
+  def with_modified_configuration
+    old_configuration = Museo.configuration
+    Museo.clear_configuration!
+    yield
+  ensure
+    Museo.instance_variable_set(:@configuration, old_configuration)
   end
 end
